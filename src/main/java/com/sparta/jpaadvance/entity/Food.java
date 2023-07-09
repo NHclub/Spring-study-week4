34b528e19b6de3954fb70396e4a4675d5ff31e0f
NHclub <sbe4488@naver.com>
2023-07-09 15:15:19 +0900

    지연 로딩과 즉시 로딩

--- src/main/java/com/sparta/jpaadvance/entity/Food.java
+++ src/main/java/com/sparta/jpaadvance/entity/Food.java
@@ -1,6 +1,7 @@
 package com.sparta.jpaadvance.entity;
 
 import jakarta.persistence.*;
+import jakarta.persistence.criteria.Fetch;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -18,6 +19,7 @@ public class Food {
     private String name;
     private double price;
 
-    @OneToMany(mappedBy = "food")
-    private List<Order> orderList = new ArrayList<>();
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 }
